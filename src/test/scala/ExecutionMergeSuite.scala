3b6cf29e3fb3f89470e04d29a9fd8dd59147c803
Roman Useinov <roman.useinov@toptal.com>
2015-01-27 10:50:03 +0100

    changed some things around

--- src/test/scala/ExecutionMergeSuite.scala
+++ src/test/scala/ExecutionMergeSuite.scala
@@ -4,41 +4,46 @@ import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
 import com.typesafe.config.{ConfigFactory, Config}
+import json.SnakifiedJsonSupport
 import org.scalatest.{BeforeAndAfter, FunSuite, Suite, Stepwise}
 import spray.can.Http
 import spray.http.{StatusCodes, StatusCode, HttpResponse, HttpRequest}
 import spray.client.pipelining._
 import com.typesafe.config
 import spray.httpx.SprayJsonSupport._
-import spray.json.DefaultJsonProtocol
 
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
-
-
-class ExecutionMergeSuite extends FunSuite with BeforeAndAfter{
-
-  case class Deploy(attachTo: String, blueprint: String, clusters: Map[String, Int])
-  case class ProxyConfig(frontends: Vector[Int], backends: Vector[Int], services: Vector[Int])
-
-  val proxyCleanup = ProxyConfig(Vector.empty, Vector.empty, Vector.empty)
-
-  object ExecutionJsonProtocol extends DefaultJsonProtocol {
-    implicit val deployFormat = jsonFormat3(Deploy)
-    implicit val proxyConfigFormat = jsonFormat3(ProxyConfig)
-  }
-
-  import ExecutionJsonProtocol._
-
-  private def assertStatusOk(response: HttpResponse) = {
+trait CustomAsserts {
+  protected def assertStatusOk(response: HttpResponse) = {
     val res = response.status match {
       case StatusCodes.Success(_) => true
       case _ => false
     }
     assert(res)
   }
+}
+
+case class Deploy(attachTo: String, blueprint: String, clusters: Map[String, Int])
+case class ProxyConfig(frontends: Vector[Int], backends: Vector[Int], services: Vector[Int])
+
+object ExecutionJsonProtocol extends SnakifiedJsonSupport {
+  implicit val deployFormat = jsonFormat3(Deploy)
+  implicit val proxyConfigFormat = jsonFormat3(ProxyConfig)
+}
+
+
+
+
+class ExecutionMergeSuite extends FunSuite with BeforeAndAfter with CustomAsserts {
+  import ExecutionJsonProtocol._
+
+  val proxyCleanup = ProxyConfig(Vector.empty, Vector.empty, Vector.empty)
+  lazy val initialDeployment = Deploy(config.getString("initialBlueprint"), "", Map.empty)
+  lazy val mergedBlueprint = config.getString("mergedBlueprint")
+
   implicit val system = ActorSystem()
   import system.dispatcher
 
@@ -69,15 +74,18 @@ class ExecutionMergeSuite extends FunSuite with BeforeAndAfter{
     val proxyReq = Get("/v1/config")
     val vampReq = Get("/")
     val cleanupReq = Post("/v1/config", proxyCleanup)
+    val deployReq = Post("/deployment", initialDeployment)
 
     assertStatusOk(Await.result(proxyPipeline(proxyReq), awaitDuration))
     info(s"Check if proxy is online")
+
     assertStatusOk(Await.result(vampPipeline(vampReq), awaitDuration))
     info("Check if vamp is online")
+
     assertStatusOk(Await.result(proxyPipeline(cleanupReq), awaitDuration))
     info("Cleanup proxy")
 
-
+    assertStatusOk(Await.result(vampPipeline(deployReq), awaitDuration))
 
   }
 
