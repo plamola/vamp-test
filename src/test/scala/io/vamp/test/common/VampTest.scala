4e5c843b7089ecaeb49b0201a6fc55b831588807
Matthijs Dekker <github@plamola.com>
2015-08-18 09:29:02 +0200

    Removed Webbrowser library dependency

--- src/test/scala/io/vamp/test/common/VampTest.scala
+++ src/test/scala/io/vamp/test/common/VampTest.scala
@@ -1,10 +1,9 @@
 package io.vamp.test.common
 
 import org.scalatest.FeatureSpec
-import org.scalatest.selenium.WebBrowser
 import traits.FileAccess
 
 
-trait VampTest extends FeatureSpec with FileAccess with WebBrowser {
+trait VampTest extends FeatureSpec with FileAccess  {
 
 }
